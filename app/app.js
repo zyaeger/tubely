62d0cc5fc78986d11841d03de8cb7a3bedf3ed91
zyaeger <zachyaeger@gmail.com>
2025-10-16 13:29:07 -0700

    update js to cache bust

--- app/app.js
+++ app/app.js
@@ -261,7 +261,7 @@ function viewVideo(video) {
     thumbnailImg.style.display = 'none';
   } else {
     thumbnailImg.style.display = 'block';
-    thumbnailImg.src = video.thumbnail_url;
+    thumbnailImg.src = `${video.thumbnail_url}?v=${Date.now()}`;
   }
 
   const videoPlayer = document.getElementById('video-player');
